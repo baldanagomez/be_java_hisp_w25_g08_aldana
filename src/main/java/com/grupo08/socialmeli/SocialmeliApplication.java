dcf54767e96c5c60e878264fd2eda11a48f39ec7
Andres Madera <andresmaderagarcia@gmail.com>
2024-02-16 22:52:50 -0500

    Segundo commit

--- src/main/java/com/grupo08/socialmeli/SocialmeliApplication.java
+++ src/main/java/com/grupo08/socialmeli/SocialmeliApplication.java
@@ -1,4 +1,4 @@
-package com.meli.socialmeli;
+package com.grupo08.socialmeli;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
