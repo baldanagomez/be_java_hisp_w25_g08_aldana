004be4cb8874d9437bb8e027d5d6d756ce0f4650
Andres Madera <andresmaderagarcia@gmail.com>
2024-02-19 15:40:35 -0500

    + Post Exceptions

--- src/main/java/com/grupo08/socialmeli/service/PostServiceImp.java
+++ src/main/java/com/grupo08/socialmeli/service/PostServiceImp.java
@@ -1,6 +1,7 @@
 package com.grupo08.socialmeli.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,14 +9,21 @@ import org.springframework.stereotype.Service;
 import com.grupo08.socialmeli.dto.ExceptionDto;
 import com.grupo08.socialmeli.dto.PostDto;
 import com.grupo08.socialmeli.entity.Post;
-import com.grupo08.socialmeli.repository.PostRepositoryImp;
+import com.grupo08.socialmeli.entity.Seller;
+import com.grupo08.socialmeli.exception.AlreadyExistException;
+import com.grupo08.socialmeli.exception.NotFoundException;
+import com.grupo08.socialmeli.repository.IPostRepository;
+import com.grupo08.socialmeli.repository.ISellerRepository;
 import com.grupo08.socialmeli.utils.PostMapper;
 
 @Service
 public class PostServiceImp implements IPostService {
 
     @Autowired
-    PostRepositoryImp postRepository;
+    IPostRepository postRepository;
+
+    @Autowired
+    ISellerRepository sellerRepository;
 
     @Override
     public List<PostDto> getAll() {
@@ -25,9 +33,20 @@ public class PostServiceImp implements IPostService {
 
     @Override
     public ExceptionDto insertPost(PostDto postDto){
+        Optional<Seller> getSeller = sellerRepository.findById(postDto.getUserId());
+
+        if (!getSeller.isPresent())
+            throw new NotFoundException("No existe vendedor");
+
+        Optional<Post> getPostbyProduct = postRepository.getPostByProductId(postDto.getProduct().getProductId());
+
+        if (!getPostbyProduct.isPresent())
+            throw new AlreadyExistException("Ya existe un producto");
+
         Post post = PostMapper.fromDto(postDto);
         postRepository.insertPost(post);
 
         return new ExceptionDto("Todo Ok");
     }
+    
 }
