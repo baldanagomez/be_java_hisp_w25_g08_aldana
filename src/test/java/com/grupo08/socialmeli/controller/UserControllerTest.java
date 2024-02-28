8f315fa0b9f9e317df81b4e283841b9db780b42a
Brayan Steven Aldana <brayansteven.aldanagomez@mercadolibre.com.co>
2024-02-28 11:41:30 -0500

    test added for t0003

--- src/test/java/com/grupo08/socialmeli/controller/UserControllerTest.java
+++ src/test/java/com/grupo08/socialmeli/controller/UserControllerTest.java
@@ -1,10 +1,27 @@
 package com.grupo08.socialmeli.controller;
 
+import com.grupo08.socialmeli.dto.response.FollowedDTO;
+import com.grupo08.socialmeli.service.IUserService;
+import com.grupo08.socialmeli.utils.TestData;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class UserControllerTest {
+
+    @Mock
+    IUserService sellerService;
+
+    @InjectMocks
+    UserController userController;
 
     @Test
     void followSeller() {
@@ -20,6 +37,20 @@ class UserControllerTest {
 
     @Test
     void getFollowedSellers() {
+        //ARRANGE
+        int userId = 1000;
+        String order = null;
+
+        FollowedDTO followedDTO = TestData.getFollowedDTOResponse(userId);
+        when(sellerService.getFollowedSellers(userId,order)).thenReturn(followedDTO);
+
+        //ACT
+        ResponseEntity<?> response =
+                userController.getFollowedSellers(followedDTO.getUserId(),order);
+
+        //ASSERT
+        assertNotNull(response);
+        assertEquals(HttpStatus.OK,response.getStatusCode());
     }
 
     @Test
